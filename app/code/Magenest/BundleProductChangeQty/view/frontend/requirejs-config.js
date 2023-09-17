0b7f2dbd833e7513b93a659fc577f55069567a2a
Ky Hai <kyhai315@gmail.com>
2023-09-17 10:12:18 +0700

    ChangeQtyBundleProduct

---
+++ app/code/Magenest/BundleProductChangeQty/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Bundle/js/price-bundle': {
+                'Magenest_BundleProductChangeQty/js/price-bundle': true
+            }
+        }
+    }
+};
